70e034c7711aba5dd5e3ae3ebfa0d510ac57b554
ThanhPT <deejay.crv@gmail.com>
2023-05-28 19:23:34 +0700

    Added 2024 version

--- RevitUtils/Extensions/IEnumratorExtensions.cs
+++ RevitUtils/Extensions/IEnumratorExtensions.cs
@@ -11,7 +11,17 @@
             }
             return list;
         }
-        
+
+        public static List<T> ToList<T>(this IEnumerator e)
+        {
+            var list = new List<T>();
+            while (e.MoveNext())
+            {
+                list.Add((T)e.Current);
+            }
+            return list;
+        }
+
         public static void AddRange<T>(this IList<T> e,List<T> list)
         {
             foreach(T item in list)
--- RevitUtils/Properties/AssemblyInfo.cs
+++ RevitUtils/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("23.02.06.0")]
-[assembly: AssemblyFileVersion("23.03.20.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("23.05.28.0")]
