3fa6a0f4efe429c48bc3e77d79047ca3262e15bf
Thanh Phan <thanh.phan@prima-sol.com>
2023-12-06 15:56:10 +0700

    Add detail message

---
+++ UI/DetailMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WT
+{
+    public class DetailMessage
+    {
+        public static DialogResult Show(string Title, string Message, string Detail)
+        {
+            // Get reference to the dialog type.
+            var dialogTypeName = "System.Windows.Forms.PropertyGridInternal.GridErrorDlg";
+            var dialogType = typeof(Form).Assembly.GetType(dialogTypeName);
+
+            // Create dialog instance.
+            var dialog = (Form)Activator.CreateInstance(dialogType, new PropertyGrid());
+
+            // Populate relevant properties on the dialog instance.
+            dialog.Text = Title;
+            dialogType.GetProperty("Details").SetValue(dialog, Detail, null);
+            dialogType.GetProperty("Message").SetValue(dialog, Message, null);
+
+            // Display dialog.
+            return dialog.ShowDialog();
+        }
+    }
+}
